b9f40308a9ab1c5c80fe1466248fd5eb87d5bcca
Daniil Golovenko (CODER) <d.golovenko@itcg.ua>
2019-05-23 00:36:32 +0300

    Updates

---
+++ precache-manifest.3d51670abb286ce58e5113768e46acf9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "052338c15f0cfa810fb3",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime~main.a33a0e9d.js"
+  },
+  {
+    "revision": "6a3c8179b1b9a8335bdd",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.35e0180d.chunk.js"
+  },
+  {
+    "revision": "e04a1a2db5a8bb8e336a",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.d0cd6762.chunk.js"
+  },
+  {
+    "revision": "6a3c8179b1b9a8335bdd",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.7d641f86.chunk.css"
+  },
+  {
+    "revision": "6e202e4029725209d643b5b384bfffe8",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  }
+];
